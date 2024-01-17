c6846157406d3da166cdcfa058eec4fdce676001
ravindra0309 <139247555+ravindra0309@users.noreply.github.com>
2024-01-17 22:28:09 +0530

    Update index.js

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://kravi0309900:iitfkaENhrEARkfF@cluster0.vxbfzhe.mongodb.net/course_selling_app');
+mongoose.connect('mongodb+srv://username:password/course_selling_app');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
